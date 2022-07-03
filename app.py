a63353b58e2590e8a4d827b865505e6100d497a2
abhishek0220 <abhishekchaudhary0220@gmail.com>
2022-07-03 14:48:39 +0530

    host changed

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def runserver():
 app = runserver()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level=logging.INFO)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level=logging.INFO)
